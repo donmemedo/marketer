972983cd129614935348319aebaf882105e4074f
MohammadAmin Khataei <m.khatayi@tech1a.co>
2023-03-12 12:37:04 +0330

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     VERSION = "0.0.1"
-    MONGO_CONNECTION_STRING = "mongodb://root:1qaz1qaz@192.17.240.40:27017/"
+    MONGO_CONNECTION_STRING = "mongodb://root:1qaz1qaz@localhost:27017/"
     MONGO_DATABASE = "brokerage"
     SWAGGER_TITLE = "Marketer API"
     API_PREFIX = ""
